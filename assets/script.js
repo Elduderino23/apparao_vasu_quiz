10871bc069c71a4c22ab8bc0410e8fd493340919
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-01 21:06:41 -0700

    remove elements from index.html and make alert box for style.js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,35 @@
-
-
-
 var startBtn = document.querySelector("#start_btn");
-function startBtn() {
+var countDown = document.getElementById("#timer");
+var timeQuestion = document.getElementById("#question");
+function startQuiz() {
     let startBtn = alert("This is a timed multiple choice quiz that tests your knowledge in code. for every incorrect question, six seconds will be subtracted from the timer. Good luck.")
     if (startBtn = start) {
         return alert(start)
+
+
 
     }
 
 }
 
+// function countdown() {
+//     var timeLeft = 5;
 
-startBtn.addEventListener("click", startBtn);
+//     var timeInterval = setInterval(function () {
+//         if (timeLeft > 1) {
+//             timerEl.textContent = timeLeft + ' seconds remaining';
+//             timeLeft--;
+//         } else if (timeLeft === 1) {
+//             timerEl.textContent = timeLeft + ' second remaining';
+//             timeLeft--;
+//         } else {
+//             timerEl.textContent = '';
+//             clearInterval(timeInterval);
+
+//             displayMessage();
+//         }
+//     }, 1000);
+// }
+
+
+startBtn.addEventListener("click", startQuiz);
