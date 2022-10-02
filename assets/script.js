f558059ccc52d68428acf07d6fe67fd5b365ae8e
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-01 18:45:50 -0700

    change tags in index.html and start script.js

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+
+
+
+var startBtn = document.querySelector("#start");
+function startBtn() {
+    let startBtn = alert("This is a timed multiple choice quiz that tests your knowledge in code. for every incorrect question, six seconds will be subtracted from the timer. Good luck.")
+    if (startBtn = start) {
+        return alert(start)
+
+    }
+
+}
+
+
+startBtn.addEventListener("click", startBtn);
